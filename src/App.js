7463e48782e1689f445c4e6d3ea4d4e494f4fb5b
Oleg <olegc601@gmail.com>
2026-03-29 17:18:51 +0300

    2.2

--- src/App.js
+++ src/App.js
@@ -30,10 +30,13 @@ function App() {
     <div className="App">
       <h1>Ежедневник лекарств</h1>
       {medicines.map(med => (
-        <div key={med.id}>
+        <div style={{
+          width: '0 auto',
+          margin: '0 auto'
+        }} key={med.id}>
         <span style={{
           textDecoration: med.taken ? 'line-through' : 'none',
-          color: med.taken ? 'green' : 'black'
+          color: med.taken ? 'green' : 'white'
         }}>
           {med.name}
         </span>
